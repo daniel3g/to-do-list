93665ca64557da89519545592b6218dbbde37619
daniel3g <silvadaniel2006@yahoo.com.br>
2022-10-18 22:59:30 -0300

    secound commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,48 @@ const inputToDo = document.querySelector('.input-to-do');
 const buttonToDo = document.querySelector('.button-to-do');
 const toDo = document.querySelector('.to-do');
 
+function createLi(){
+    const li = document.createElement('li');
+    return li;
+}
+
+function clearInput(){
+    inputToDo.value = '';
+    inputToDo.focus();
+}
+
+function createButtonClear(li){
+    li.innerText += ' ';
+    const buttonClear = document.createElement('button');
+    buttonClear.innerText = 'Apagar';
+    buttonClear.setAttribute('class', 'clear');
+    li.appendChild(buttonClear);
+}
+
+function createToDo(textToDo){
+    const li = createLi();
+    li.innerText = textToDo;
+    toDo.appendChild(li);
+    createButtonClear(li);
+}
+
+inputToDo.addEventListener('keypress', function(e) {
+      if(e.keyCode === 13){
+        if (!inputToDo.value) return;
+        createToDo(inputToDo.value);
+        clearInput();
+    }
+});
+
 buttonToDo.addEventListener('click', function(e) {
     if (!inputToDo.value) return;
-    console.log(inputToDo.value);
+    createToDo(inputToDo.value);
+    clearInput();
+});
+
+document.addEventListener('click', function(e) {
+    const el = e.target;
+    if(el.classList.contains('clear')) {
+        el.parentElement.remove();
+    }
 });
